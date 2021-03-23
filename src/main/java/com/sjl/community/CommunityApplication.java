026bdf35a696790907852cb1e15572dab35ef313
graylogo <graylogo@gmail.com>
2021-03-23 19:07:19 +0800

    更新

--- src/main/java/com/sjl/community/CommunityApplication.java
+++ src/main/java/com/sjl/community/CommunityApplication.java
@@ -7,12 +7,15 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+//标注这个类是一个springboot应用
+@SpringBootApplication
+@MapperScan("com.sjl.community.mapper")
 @EnableScheduling
 @EnableAsync
 @MapperScan("com.sjl.community.mapper")
 @SpringBootApplication
 public class CommunityApplication {
-
+//        启动springboot应用
     public static void main(String[] args) {
         SpringApplication.run(CommunityApplication.class, args);
     }
